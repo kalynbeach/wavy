5203cd6c16de681f0b492128069762baee5b7b5c
Kalyn Beach <kalynbeach@gmail.com>
2020-05-06 18:07:57 -0700

    Enable devtools config during app bootstrapping

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue';
 import store from './store'
 import App from './App.vue'
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App)
+
+app.config.devtools = true
+
+app.use(store)
+
+app.mount('#app')
