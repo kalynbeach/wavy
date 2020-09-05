76efc02a18588eee0b039f4ac16e8af9d9847fdc
Kalyn Beach <kalynbeach@gmail.com>
2020-09-05 12:50:19 -0700

    Add some Canvas styles

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="canvas">
-    <h2>Canvas</h2>
     <canvas id="canvas"></canvas>
   </div>
 </template>
@@ -11,10 +10,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .canvas {
   width: 100%;
   min-height: 95vh;
   background-color: rgb(255, 255, 255);
+
+  #canvas {
+    width: 100%;
+    height: 100%;
+    border: 2px solid #2c3e50;;
+    box-sizing: border-box;
+  }
 }
 </style>
